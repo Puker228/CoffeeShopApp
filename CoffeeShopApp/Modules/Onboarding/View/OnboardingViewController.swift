2ccfe008865f9a3c6f8a02908a40d7e624952f58
Danil <karatelanalov@yandex.ru>
2025-06-18 23:42:37 +0300

    updated img

--- CoffeeShopApp/Modules/Onboarding/View/OnboardingViewController.swift
+++ CoffeeShopApp/Modules/Onboarding/View/OnboardingViewController.swift
@@ -16,41 +16,59 @@ class OnboardingViewController: UIViewController {
         return $0
     }(UIImageView())
     
-    lazy var mainTitleLable: UILabel = createLabel(text: "Fall in Love with Coffee in Blissful Delight!", size: 36, weight: .bold)
+    lazy var getStartedButton: UIButton = {
+        $0.setTitle("Get Started", for: .normal)
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        $0.backgroundColor = .coffee01
+        $0.setTitleColor(.white, for: .normal)
+        $0.layer.cornerRadius = 16
+        $0.clipsToBounds = true
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIButton())
     
-    lazy var subTitleLable: UILabel = createLabel(text: "Welcome to our cozy coffee corner, where every cup is a delightful for you.", size: 18, weight: .regular)
+    lazy var mainTitleLable: UILabel = createLabel(text: "Fall in Love with Coffee in Blissful Delight!", size: 36, weight: .bold, textColor: .white)
+    
+    lazy var subTitleLable: UILabel = createLabel(text: "Welcome to our cozy coffee corner, where every cup is a delightful for you.", size: 18, weight: .regular, textColor: .coffeeSubTitle)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .green
         
         view.addSubview(onboardingImageView)
         view.addSubview(mainTitleLable)
         view.addSubview(subTitleLable)
+        view.addSubview(getStartedButton)
         
         NSLayoutConstraint.activate([
-            mainTitleLable.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            mainTitleLable.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            mainTitleLable.bottomAnchor.constraint(equalTo: subTitleLable.topAnchor, constant: -10),
+            mainTitleLable.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            mainTitleLable.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+            mainTitleLable.bottomAnchor.constraint(equalTo: subTitleLable.topAnchor, constant: -4),
             
-            subTitleLable.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            subTitleLable.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            subTitleLable.topAnchor.constraint(equalTo: mainTitleLable.bottomAnchor, constant: 8),
-            subTitleLable.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            subTitleLable.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            subTitleLable.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+            subTitleLable.topAnchor.constraint(equalTo: mainTitleLable.bottomAnchor, constant: 4),
+            subTitleLable.bottomAnchor.constraint(equalTo: getStartedButton.topAnchor, constant: -20),
             
-            onboardingImageView.topAnchor.constraint(equalTo: view.topAnchor),
-            onboardingImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            onboardingImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            onboardingImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            getStartedButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            getStartedButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+            getStartedButton.topAnchor.constraint(equalTo: subTitleLable.bottomAnchor, constant: 20),
+            getStartedButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -54),
+            getStartedButton.heightAnchor.constraint(equalToConstant: 56),
+            
+            onboardingImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: -166),
+            onboardingImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -47),
+            onboardingImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 46),
+            onboardingImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -276),
         ])
     }
     
-    func createLabel(text: String, size: Int, weight: UIFont.Weight) -> UILabel {
+    func createLabel(text: String, size: Int, weight: UIFont.Weight, textColor: UIColor) -> UILabel {
         let label = UILabel()
         label.text = text
         label.font = UIFont.systemFont(ofSize: CGFloat(size), weight: weight)
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.textColor = .white
+        label.textColor = textColor
         label.textAlignment = .center
         label.numberOfLines = 0
         return label

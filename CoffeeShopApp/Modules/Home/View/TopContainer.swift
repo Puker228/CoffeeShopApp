865d4a572c9d497172fd9c170608494687331b28
Danil <karatelanalov@yandex.ru>
2025-06-29 00:01:57 +0300

    started making up a new screen

---
+++ CoffeeShopApp/Modules/Home/View/TopContainer.swift
@@ -0,0 +1,34 @@
+//
+//  TopContainer.swift
+//  CoffeeShopApp
+//
+//  Created by Данил Дьяченко on 28.06.2025.
+//
+
+import UIKit
+
+final class TopContainer: UIView {
+    private let gradientLayer = CAGradientLayer()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupGradient()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupGradient()
+    }
+    
+    private func setupGradient() {
+        gradientLayer.colors = [UIColor.systemBlue.cgColor, UIColor.systemPurple.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0, y: 0.44)
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = bounds
+    }
+}
--- CoffeeShopApp/Modules/Home/ViewController/HomeViewController.swift
+++ CoffeeShopApp/Modules/Home/ViewController/HomeViewController.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    let gradientView = TopContainer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .green
         navigationItem.hidesBackButton = true
+        
+        setupGradientView()
+    }
+    
+    private func setupGradientView() {
+        gradientView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(gradientView)
+
+        NSLayoutConstraint.activate([
+            gradientView.topAnchor.constraint(equalTo: view.topAnchor),
+            gradientView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            gradientView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            gradientView.heightAnchor.constraint(equalToConstant: 200)
+        ])
     }
 }
